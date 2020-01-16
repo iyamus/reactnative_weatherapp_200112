8559aa22af23a61e44a1c61e93ba0f621f0ef2fe
iyamus <iyamus@gmail.com>
2020-01-16 00:10:19 -0700

    final version

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
-import React from "react";
-import { Alert } from "react-native";
+import React from 'react';
 import Loading from "./Loading";
+import { Alert, TouchableNativeFeedbackBase } from "react-native";
 import * as Location from "expo-location";
-import axios from "axios";
+import axios from 'axios';
 import Weather from "./Weather";
 
-const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
+//Weather API key
+const API_KEY = "afff68bddf85b21a1b24ff8a7623b434";
 
-export default class extends React.Component {
+export default class App extends React.Component {
   state = {
     isLoading: true
-  };
+
+  }
   getWeather = async (latitude, longitude) => {
     const {
       data: {
@@ -30,27 +32,40 @@ export default class extends React.Component {
       condition: weather[0].main
     });
   };
+
+
   getLocation = async () => {
     try {
-      await Location.requestPermissionsAsync();
-      const {
-        coords: { latitude, longitude }
-      } = await Location.getCurrentPositionAsync();
+      // throw Error(); catch 영역으로 강제로 이동시킴.
+      //현재 위치 정보 사용 요청하기
+      const response = Location.requestPermissionsAsync();
+      //전체 결과값 가져오기
+      const location = await Location.getCurrentPositionAsync();
+      // 지역 관련 객체 정보 추출하기
+      const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
+      // 날씨 정보 가져오는 함수 호출
       this.getWeather(latitude, longitude);
+
     } catch (error) {
-      Alert.alert("Can't find you.", "So sad");
+      Alert.alert("Cannot find where you are.");
     }
-  };
+  }
+
   componentDidMount() {
     this.getLocation();
   }
+
   render() {
     const { isLoading, temp, temp_max, temp_min, name, condition } = this.state;
+    // const { isLoading, temp, condition } = this.state;
 
     return isLoading ? (
       <Loading />
-    ) : (
-        <Weather temp={Math.round(temp)} temp_max={Math.round(temp_max)} temp_min={Math.round(temp_min)} name={name} condition={condition} />
+      ) : (
+      // <Weather temp={Math.round(temp)} condition={condition} />
+      <Weather temp={Math.round(temp)} temp_max={Math.round(temp_max)} temp_min={Math.round(temp_min)} name={name} condition={condition} />
       );
+
   }
 }
+
